c5ba42cb807cde7f3ff4092b40dc7166196d6120
sharmh34 <71384247+sharmh34@users.noreply.github.com>
2020-09-17 17:36:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,5 +8,5 @@ var server = http.createServer(function (request, response) {
 });
 
 // listen on localhost:8000
-server.listen(8000);
-console.log("Server listening on port 8000 :  http://127.0.0.1:8000/");
+server.listen(5432);
+console.log("Server listening on port 5432 :  http://127.0.0.1:5432/");
